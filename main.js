1852189c125a73405ba0fa2d38743750882ceaad
aninhamendes10 <163137027+aninhamendes10@users.noreply.github.com>
2024-03-12 09:40:26 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+
+function mudarimagemfora()
+{
+    document.getElementById("imagem").src ="https://www.rbsdirect.com.br/imagesrc/25502140.jpg?w=1024&h=1024&a=c";
+}
+
+function mudarimagemdentro()
+{
+    document.getElementById("imagem").src ="https://i.pinimg.com/736x/79/9c/b7/799cb79b494fbfac783555cf9acbebfe.jpg";
+}
